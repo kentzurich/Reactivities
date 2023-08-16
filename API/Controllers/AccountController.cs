5c161b6fe368d39022db1062188c02c8fad61c46
kentzurich <kentzurichanthony@gmail.com>
2023-08-16 10:16:49 +0800

    Adding the confirm email endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -90,16 +90,37 @@ namespace API.Controllers
             
             if(!result.Succeeded) return BadRequest("Problem registering user.");
 
-            var origin = Request.Headers["origin"];
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
-
-            var verifyUrl = $"{origin}/account/verifyEmail?token={token}&email={user.Email}";
-            var message = $"<p>Please click the below link to verify your email address: </p><p><a href='{verifyUrl}'>Click to verify email</a></p>";
-
-            await _emailSender.SendEmailAsync(user.Email, "Please verify email", message);
+            await SendEmail(user);
 
             return Ok("Registration success - please verify email.");
+        }
+
+        [AllowAnonymous]
+        [HttpPost("verifyEmail")]
+        public async Task<IActionResult> VerifyEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if(user == null) return Unauthorized();
+
+            var decodedTokenBytes = WebEncoders.Base64UrlDecode(token);
+            var decodedToken = Encoding.UTF8.GetString(decodedTokenBytes);
+            var result = await _userManager.ConfirmEmailAsync(user, decodedToken);
+
+            if(!result.Succeeded) return BadRequest("Could not verify email address.");
+
+            return Ok("Email confirmed. You can now login.");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("resendEmailConfirmationLink")]
+        public async Task<IActionResult> ResendConfirmationLink(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if(user == null) return Unauthorized();
+
+            await SendEmail(user);
+
+            return Ok("Email verification link resent.");
         }
 
         [Authorize]
@@ -179,6 +200,18 @@ namespace API.Controllers
 
             return CreateUserObject(user);
         } 
+
+        private async Task SendEmail(AppUser user)
+        {
+            var origin = Request.Headers["origin"];
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+            var verifyUrl = $"{origin}/account/verifyEmail?token={token}&email={user.Email}";
+            var message = $"<p>Please click the below link to verify your email address: </p><p><a href='{verifyUrl}'>Click to verify email</a></p>";
+
+            await _emailSender.SendEmailAsync(user.Email, "Please verify email", message);
+        }
 
         private async Task SetRefreshToken(AppUser user)
         {
